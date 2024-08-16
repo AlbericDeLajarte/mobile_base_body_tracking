4a01de108dfc4575f5e5ce0ed94e86d722c99c4c
AlbericDeLajarte <albericlajarte@gmail.com>
2024-08-16 16:46:09 -0700

    small fix

--- setup/server_config.sh
+++ setup/server_config.sh
@@ -11,11 +11,15 @@ CHANNEL="7"
 
 # Update and install necessary packages
 apt update
-apt install -y hostapd dnsmasq iptables-persistent network-manager
+apt install -y hostapd iptables-persistent network-manager
+
+# Stop and disable dnsmasq (since we will not be using it)
+systemctl stop dnsmasq
+systemctl disable dnsmasq
+systemctl mask dnsmasq
 
 # Stop services while configuring
 systemctl stop hostapd
-systemctl stop dnsmasq
 
 # Configure hostapd
 cat > /etc/hostapd/hostapd.conf <<EOL
@@ -38,19 +42,11 @@ EOL
 # Point hostapd to the configuration file
 sed -i 's|#DAEMON_CONF="|DAEMON_CONF="/etc/hostapd/hostapd.conf|' /etc/default/hostapd
 
-# Configure dnsmasq
-mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
-cat > /etc/dnsmasq.conf <<EOL
-interface=$NET_INTERFACE
-dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h
-EOL
-
-# Configure static IP for wlan0 using NetworkManager
+# Configure NetworkManager to manage the hotspot
 nmcli connection add type wifi ifname $NET_INTERFACE con-name "Hotspot" autoconnect yes ssid $SSID
 nmcli connection modify "Hotspot" 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
 nmcli connection modify "Hotspot" wifi-sec.key-mgmt wpa-psk wifi-sec.psk $PASSWORD
 nmcli connection modify "Hotspot" ipv4.addresses $STATIC_IP/24
-nmcli connection modify "Hotspot" ipv4.gateway $STATIC_IP
 
 # Enable IP routing
 sed -i 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|' /etc/sysctl.conf
@@ -63,12 +59,10 @@ sh -c "iptables-save > /etc/iptables.ipv4.nat"
 # Ensure iptables rule is restored on boot
 sed -i '$ i\iptables-restore < /etc/iptables.ipv4.nat' /etc/rc.local
 
-# Enable and start the hostapd and dnsmasq services
+# Enable and start the hostapd service
 systemctl unmask hostapd
 systemctl enable hostapd
-systemctl enable dnsmasq
 systemctl start hostapd
-systemctl start dnsmasq
 
 # Start the NetworkManager connection
 nmcli connection up "Hotspot"
